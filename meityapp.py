65c3e2c6b7be6fd50880096cded3b1c63ad184e6
KEERTHI U <75976487+KEERTHI1912@users.noreply.github.com>
2023-09-01 04:23:38 +0530

    Update meityapp.py

--- meityapp.py
+++ meityapp.py
@@ -1,4 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
+import sqlite3
 from streamlit_chat import message
 from embedchain import OpenSourceApp
 
